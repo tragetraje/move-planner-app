1b55365b71d8aee232c0d819dd60f4687ac1718c
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-08-11 21:37:22 +1000

    Append street view image from API

--- js/script.js
+++ js/script.js
@@ -11,9 +11,21 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    // load streetview:
+    // collect the values of street address and city of submit form
+    var streetVal = $( "#street" ).val();
+    var cityVal = $( "#city" ).val();
+    var location = cityVal + ' ,' + streetVal;
 
-    // YOUR CODE GOES HERE!
+    // street view url
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + location + '&key=AIzaSyAU_HCBE5IojEje5VGo95e7izIupvhmqHE';
+
+    // append the image on the page
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+
+
 
     return false;
 };
